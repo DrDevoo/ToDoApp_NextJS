9662af9d068d13130bba50ff05b69d68434ce4f7
K_Richard <krichard001@icloud.com>
2024-09-02 13:44:39 +0200

    todo list design

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,25 @@
-import Image from "next/image";
 import Link from 'next/link';
+import './styles/home.css'
 
 export default function Home() {
   return (
     <main>
       <header><nav className=""></nav></header>
-      <section className="">
-        
+      <section className="hero">
+        <h1 className=""><center>To-Do feldatkezelő alkalmazás</center></h1>
+      </section>
+      <section className="todolist">
+        <div className="todo">
+          <span className="">todo neve</span>
+          <input type="checkbox" id="id" name="name" className="hidden" />
+        </div>
+        <div className="todo">
+          <span className="">todo neve</span>
+          <input type="checkbox" id="id" name="name" className="hidden" />
+        </div>
+        <div className="addtodo">
+          <span className="">+</span>
+        </div>
       </section>
     </main>
   );
