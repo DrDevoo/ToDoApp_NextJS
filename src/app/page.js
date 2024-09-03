fa56006da5d760752dcefd13d073f35a7cbdbccc
K_Richard <krichard001@icloud.com>
2024-09-03 09:22:41 +0200

    keresés, rendezés, design

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+//Code by: Kis Richárd
 "use client";
 import Link from 'next/link';
 import './styles/home.css'
@@ -5,17 +6,20 @@ import './styles/global.css'
 import { useState, useEffect } from 'react';
 
 export default function Home() {
+  //Betöltéskor feladatok lekérése
   const [todos, setTodos] = useState(() => {
     const storedTodos = localStorage.getItem('todos');
     return storedTodos ? JSON.parse(storedTodos) : [];
   });
 
+  //Feladatok mentése ha változás történik
   useEffect(() => {
-    if (todos.length > 0) {
+    if (todos.length >= 0) {
       localStorage.setItem('todos', JSON.stringify(todos));
     }
   }, [todos]);
 
+  //Feladat állapotának frissítése
   const toggleTodo = (id) => {
     const updatedTodos = todos.map((todo) =>
       todo.id === id ? { ...todo, completed: !todo.completed } : todo
@@ -25,12 +29,43 @@ export default function Home() {
 
   const [showAdd, setShowAdd] = useState(false);
   var [newTodo, setNewTodo] = useState("");
+  var [fontossag, setFontossag] = useState(0);
+  var [hatarido, setHatarido] = useState(null);
   const addTodo = async () => {
     if (newTodo.trim() === '') return;
-    setTodos([...todos, { id: Date.now(), text: newTodo, completed: false }]);
+    setTodos([...todos, { id: Date.now(), text: newTodo, completed: false, fontossag: fontossag, hatarido: hatarido }]);
     setNewTodo('');
     setShowAdd(false)
   };
+
+  const deleteTodo = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+  const [htsort, setHtsort] = useState("completed");
+  const [kereso, setKereso] = useState('');
+  const filteredTodos = todos.filter((todo) =>
+    todo.text.toLowerCase().includes(kereso.toLowerCase())
+  );
+  const sortedTodos = [...filteredTodos].sort((a, b) => {
+    if (a.completed !== b.completed) {
+      return a.completed ? 1 : -1;
+    }
+    if (htsort === 'completed') {
+      return a.completed - b.completed;
+    } else if (htsort === 'fontontossag') {
+      return b.fontossag - a.fontossag;
+    } else if (htsort === 'hatarido') {
+      if (a.hatarido && b.hatarido) {
+        return new Date(a.hatarido) - new Date(b.hatarido);
+      }
+      if (!a.hatarido && !b.hatarido) return 0;
+      return a.hatarido ? -1 : 1;
+    }
+  });
+
+
+
 
   return (
     <main>
@@ -45,12 +80,40 @@ export default function Home() {
         <h1 className=""><center>To-Do feldatkezelő alkalmazás</center></h1>
       </section>
       <section className="todolist">
-        {todos.sort((a, b) => a.completed - b.completed).map((todo) => (
+        <div className="sorterbox">
+          <div className="searchbox">
+            <input type="search" className="" placeholder="keresés..." onChange={(e) => setKereso(e.target.value)} value={kereso}></input>
+          </div>
+          <div className="sortbox">
+            <span className="">Rendezés: </span>
+            <select className="" onChange={(e) => setHtsort(e.target.value)}>
+                <option value="completed">alap</option>
+                <option value="fontontossag">fontosság</option>
+                <option value="hatarido">határidő</option>
+            </select>
+          </div>
+        </div>
+
+        {sortedTodos.map((todo) => (
           <div key={todo.id} className={todo.completed ? 'kesz todo' : 'todo'}>
-            <span className="">{todo.text}</span>
-            <input type="checkbox" checked={todo.completed} onChange={() => toggleTodo(todo.id)} />
+            <div className="todotop">
+              <span className="">{todo.text}</span>
+              <div className="buttons">
+                <input type="checkbox" checked={todo.completed} onChange={() => toggleTodo(todo.id)} />
+                <button onClick={() => deleteTodo(todo.id)}>törlés</button>
+              </div>
+            </div>
+            <div className="todobott">
+            {todo.hatarido !== null && (
+              <span className="">Határidő: {todo.hatarido}</span>
+            )}
+              <span className="">Fontosság: {todo.fontossag}</span>
+            </div>
           </div>
         ))}
+        {sortedTodos.length === 0 && (
+          <h4 className="">Nincsenek feldatok...</h4>
+        )}
 
         <div className="addtodo" onClick={() => setShowAdd(!showAdd)}>
           <span className="">+</span>
@@ -62,6 +125,14 @@ export default function Home() {
             placeholder="feladat neve"
             onChange={(e) => setNewTodo(e.target.value)}
           />
+          <label for="fontossag">Fontosság</label>
+          <select className="" name="fontossag" onChange={(e) => setFontossag(e.target.value)}>
+              <option value="0">Nem nagyon fontos</option>
+              <option value="1">Közepesen fontos</option>
+              <option value="2">Nagyon fontos</option>
+          </select>
+          <label for="hatarido">Határidő</label>
+          <input type="date" name="hatarido" id="" value={hatarido} onChange={(e) => setHatarido(e.target.value)} className=""></input>
           <button onClick={() => addTodo()}>
             Mentés
           </button>
